e561d6aee91b1e94faf4991b34ad6a2de5bbea4d
chinmoy-kumar <chinmoy.coder@gmail.com>
2024-03-24 14:17:29 +0600

    add primeNumber_checker program

---
+++ primeNumber_checker/primeNumber_checker.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main()
+{
+    int n, a;
+    printf("Enter a number to check: ");
+    scanf("%d", &n);
+
+    a = 0;
+    for (int i = 2; i < n; i++)
+    {
+        if (n % i == 0)
+        {
+            a += 1;
+            break;
+        }
+    }
+    if (a == 0)
+    {
+        printf("%d is a prime number", n);
+    }
+    else
+    {
+        printf("%d is a composite number", n);
+    }
+
+    return 0;
+}
