263d811369273d5fe62ac51418bee70a399c68ec
chinmoy-kumar <chinmoy.coder@gmail.com>
2024-04-23 17:04:48 +0600

    added 24 & 25 no programs

---
+++ 25. alphabetSquare/alphabetSquare.c
@@ -0,0 +1,29 @@
+// problem: print the given pattern
+// A B C D E 
+// A B C D E 
+// A B C D E 
+// A B C D E 
+
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    printf("Enter the number of rows: ");
+    scanf("%d", &n);
+
+    for (int i = 1; i <= n; i++)
+    {   
+        int a = 65;
+        for (int j = 1; j <= n; j++)
+        {
+            char ch = (char)a; 
+            printf("%c ", ch);
+            a++;
+        }
+        printf("\n");
+    }
+    
+
+    return 0;
+}
